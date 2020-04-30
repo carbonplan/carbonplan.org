5ee6e855f1926f4abe07d71d021eaf016e57cb6f
freeman-lab <the.freeman.lab@gmail.com>
2020-04-29 19:34:55 -0700

    fix

--- components/seo.js
+++ components/seo.js
@@ -3,13 +3,13 @@ import Head from 'next/head'
 const Seo = () => (
   <Head>
     <title>(carbon)plan</title>
-    <meta name='description' content='Data and science for carbon removal.' />
-    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    <link rel='canonical' content='https://carbonplan.org/' />
+    <meta name='description' content='Data and science for carbon removal.'/>
+    <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+    <link rel='canonical' content='https://carbonplan.org/'/>
     <link rel='icon' href='/favicon.svg'/>
-    <link rel='apple-touch-icon' href='/favicon.png'>
-    <meta property='og:title' content='(carbon)plan' />
-    <meta property='og:image' content='/favicon.png' />
+    <link rel='apple-touch-icon' href='/favicon.png'/>
+    <meta property='og:title' content='(carbon)plan'/>
+    <meta property='og:image' content='/favicon.png'/>
   </Head>
 )
 
