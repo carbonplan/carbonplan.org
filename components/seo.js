3b2eb205d013f7a6a6b89d14c6a64c5a27ead050
freeman-lab <the.freeman.lab@gmail.com>
2020-05-01 21:54:02 -0700

    fix links

--- components/seo.js
+++ components/seo.js
@@ -6,10 +6,10 @@ const Seo = () => (
     <meta name='description' content='Data and science for carbon removal.'/>
     <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
     <link rel='canonical' content='https://carbonplan.org/'/>
-    <link rel='icon' href='https://carbonplan-assets.s3.amazonaws.com/favicon.svg'/>
-    <link rel='apple-touch-icon' href='https://carbonplan-assets.s3.amazonaws.com/favicon.png'/>
+    <link rel='icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.svg'/>
+    <link rel='apple-touch-icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.png'/>
     <meta property='og:title' content='(carbon)plan'/>
-    <meta property='og:image' href='https://carbonplan-assets.s3.amazonaws.com/social.png'/>
+    <meta property='og:image' href='https://carbonplan-assets.s3.amazonaws.com/images/social.png'/>
   </Head>
 )
 
