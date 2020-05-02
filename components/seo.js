d07866cf3dc308bed22b0ccf13e9c8383a71b7ac
freeman-lab <the.freeman.lab@gmail.com>
2020-05-01 22:10:41 -0700

    fix tags

--- components/seo.js
+++ components/seo.js
@@ -9,13 +9,13 @@ const Seo = () => (
     <link rel='icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.svg'/>
     <link rel='apple-touch-icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.png'/>
     <meta property='og:title' content='(carbon)plan'/>
-    <meta property="og:description" content="Data and science for carbon removal.">
+    <meta property="og:description" content="Data and science for carbon removal."/>
     <meta property='og:image' content='https://carbonplan-assets.s3.amazonaws.com/images/social.png'/>
-    <meta property="og:url" content="https://carbonplan.org">
-    <meta name="twitter:title" content="(carbon)plan">
-    <meta name="twitter:description" content="Data and science for carbon removal.">
-    <meta name="twitter:image" content=" https://carbonplan-assets.s3.amazonaws.com/images/social.png">
-    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:url" content="https://carbonplan.org"/>
+    <meta name="twitter:title" content="(carbon)plan"/>
+    <meta name="twitter:description" content="Data and science for carbon removal."/>
+    <meta name="twitter:image" content=" https://carbonplan-assets.s3.amazonaws.com/images/social.png"/>
+    <meta name="twitter:card" content="summary_large_image"/>
   </Head>
 )
 
