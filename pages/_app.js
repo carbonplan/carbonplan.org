ea8c1cb249fe3b3ff4da1f1ef014b5a5f737fb49
Joseph Hamman <jhamman@ucar.edu>
2020-09-11 10:36:50 -0700

    lint

--- pages/_app.js
+++ pages/_app.js
@@ -21,14 +21,15 @@ class MyApp extends App {
               text-decoration: none;
             }
 
-            sup, sub {
+            sup,
+            sub {
               vertical-align: baseline;
               position: relative;
               top: -0.4em;
             }
 
-            sub { 
-              top: 0.4em; 
+            sub {
+              top: 0.4em;
               font-size: small;
             }
 
@@ -42,10 +43,11 @@ class MyApp extends App {
             @font-face {
               font-family: 'relative-book-pro';
               src: url('${prefix}relative-book-pro.eot');
-              src: url('${prefix}relative-book-pro.eot?#iefix') format('embedded-opentype'),
-                   url('${prefix}relative-book-pro.woff2') format('woff2'),
-                   url('${prefix}relative-book-pro.woff') format('woff'),
-                   url('${prefix}relative-book-pro.ttf') format('truetype');
+              src: url('${prefix}relative-book-pro.eot?#iefix')
+                  format('embedded-opentype'),
+                url('${prefix}relative-book-pro.woff2') format('woff2'),
+                url('${prefix}relative-book-pro.woff') format('woff'),
+                url('${prefix}relative-book-pro.ttf') format('truetype');
               font-weight: normal;
               font-style: normal;
             }
@@ -53,10 +55,11 @@ class MyApp extends App {
             @font-face {
               font-family: 'relative-medium-pro';
               src: url('${prefix}relative-medium-pro.eot');
-              src: url('${prefix}relative-medium-pro.eot?#iefix') format('embedded-opentype'),
-                   url('${prefix}relative-medium-pro.woff2') format('woff2'),
-                   url('${prefix}relative-medium-pro.woff') format('woff'),
-                   url('${prefix}relative-medium-pro.ttf') format('truetype');
+              src: url('${prefix}relative-medium-pro.eot?#iefix')
+                  format('embedded-opentype'),
+                url('${prefix}relative-medium-pro.woff2') format('woff2'),
+                url('${prefix}relative-medium-pro.woff') format('woff'),
+                url('${prefix}relative-medium-pro.ttf') format('truetype');
               font-weight: normal;
               font-style: normal;
             }
@@ -64,10 +67,12 @@ class MyApp extends App {
             @font-face {
               font-family: 'relative-mono-11-pitch-pro';
               src: url('${prefix}relative-mono-11-pitch-pro.eot');
-              src: url('${prefix}relative-mono-11-pitch-pro.eot?#iefix') format('embedded-opentype'),
-                   url('${prefix}relative-mono-11-pitch-pro.woff2') format('woff2'),
-                   url('${prefix}relative-mono-11-pitch-pro.woff') format('woff'),
-                   url('${prefix}relative-mono-11-pitch-pro.ttf') format('truetype');
+              src: url('${prefix}relative-mono-11-pitch-pro.eot?#iefix')
+                  format('embedded-opentype'),
+                url('${prefix}relative-mono-11-pitch-pro.woff2') format('woff2'),
+                url('${prefix}relative-mono-11-pitch-pro.woff') format('woff'),
+                url('${prefix}relative-mono-11-pitch-pro.ttf')
+                  format('truetype');
               font-weight: normal;
               font-style: normal;
             }
@@ -75,14 +80,14 @@ class MyApp extends App {
             @font-face {
               font-family: 'relative-faux-book-pro';
               src: url('${prefix}relative-faux-book-pro.eot');
-              src: url('${prefix}relative-faux-book-pro.eot?#iefix') format('embedded-opentype'),
-                   url('${prefix}relative-faux-book-pro.woff2') format('woff2'),
-                   url('${prefix}relative-faux-book-pro.woff') format('woff'),
-                   url('${prefix}relative-faux-book-pro.ttf') format('truetype');
+              src: url('${prefix}relative-faux-book-pro.eot?#iefix')
+                  format('embedded-opentype'),
+                url('${prefix}relative-faux-book-pro.woff2') format('woff2'),
+                url('${prefix}relative-faux-book-pro.woff') format('woff'),
+                url('${prefix}relative-faux-book-pro.ttf') format('truetype');
               font-weight: normal;
               font-style: normal;
             }
-
           `}
         </style>
       </ThemeProvider>
