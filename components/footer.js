9eafb254c76c4efb11c3424ea39a16617d517c1f
freeman-lab <the.freeman.lab@gmail.com>
2020-09-28 21:21:40 -0700

    fix footnote

--- components/footer.js
+++ components/footer.js
@@ -37,8 +37,8 @@ const Footer = () => {
         </Box>
         <Box sx={{ fontFamily: 'body', letterSpacing: 'body' }}>
           <Text sx={{ color: 'secondary' }}>
-            CarbonPlan is a registered non-profit public benefit corporation in
-            California with 501(3)(c) status pending.
+            CarbonPlan is a registered non-profit public benefit corporation in 
+            California <br/>with 501(3)(c) status.
           </Text>
         </Box>
       </Grid>
