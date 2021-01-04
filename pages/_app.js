6f2f0edd54f61ac8faadb90668fbe77e161f0109
freeman-lab <the.freeman.lab@gmail.com>
2021-01-04 13:39:24 -0800

    bump dep to use css files

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import { ThemeProvider } from 'theme-ui'
-import { Fonts, Globals } from '@carbonplan/components'
 import theme from '@carbonplan/theme'
+import '@carbonplan/components/globals.css'
+import '@carbonplan/components/fonts.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
-      <Fonts />
-      <Globals />
     </ThemeProvider>
   )
 }
