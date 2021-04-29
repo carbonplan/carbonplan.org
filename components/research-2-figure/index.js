781ee8cab3c9d2f69c59fe4c41b35fed6bcbd639
freeman-lab <the.freeman.lab@gmail.com>
2021-04-28 23:29:25 -0700

    formatting

--- components/research-2-figure/index.js
+++ components/research-2-figure/index.js
@@ -27,50 +27,45 @@ const y = scaleLinear().domain([0, 1]).range([0, 30])
 const Figure = () => {
   return (
     <Row as='figure' sx={{ mt: [4, 5, 6, 7], mb: [4, 4, 4, 5] }}>
-      <Column
-        start={[1, 1, 2, 2]}
-        width={[6, 8, 10, 10]}
-      >
+      <Column start={[1, 1, 2, 2]} width={[6, 8, 10, 10]}>
         <svg
-            width='100%'
-            height='224'
-            viewBox='0 0 100 60'
-            preserveAspectRatio='none'
-          >
-            {projects.map((d, i) => {
-              const positive = d > 0
-              const fraction = Math.abs(d)
-              return (
-                <g key={'f-' + i} transform={`translate(${x(i)},0)`}>
-                  <Box
-                    as='rect'
-                    width={0.75 * (100 / count)}
-                    x='0'
-                    height={2 * (y(1) - y(0))}
-                    y={y(0)}
-                    sx={{
-                      fill: positive ? 'green' : 'secondary',
-                      pointerEvents: ['none'],
-                    }}
-                    fillOpacity={
-                     '0.2'
-                    }
-                  />
-                  <Box
-                    as='rect'
-                    width={0.75 * (100 / count)}
-                    x='0'
-                    height={y(1) - y(1 - fraction)}
-                    y={positive ? y(1 - fraction) : y(1)}
-                    sx={{
-                      fill: positive ? 'green' : 'secondary',
-                      pointerEvents: ['none'],
-                    }}
-                  />
-                </g>
-              )
-            })}
-          </svg>
+          width='100%'
+          height='224'
+          viewBox='0 0 100 60'
+          preserveAspectRatio='none'
+        >
+          {projects.map((d, i) => {
+            const positive = d > 0
+            const fraction = Math.abs(d)
+            return (
+              <g key={'f-' + i} transform={`translate(${x(i)},0)`}>
+                <Box
+                  as='rect'
+                  width={0.75 * (100 / count)}
+                  x='0'
+                  height={2 * (y(1) - y(0))}
+                  y={y(0)}
+                  sx={{
+                    fill: positive ? 'green' : 'secondary',
+                    pointerEvents: ['none'],
+                  }}
+                  fillOpacity={'0.2'}
+                />
+                <Box
+                  as='rect'
+                  width={0.75 * (100 / count)}
+                  x='0'
+                  height={y(1) - y(1 - fraction)}
+                  y={positive ? y(1 - fraction) : y(1)}
+                  sx={{
+                    fill: positive ? 'green' : 'secondary',
+                    pointerEvents: ['none'],
+                  }}
+                />
+              </g>
+            )
+          })}
+        </svg>
       </Column>
     </Row>
   )
