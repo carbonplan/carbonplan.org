ee6e251a443426e12544e4bc60726a416c124575
freeman-lab <the.freeman.lab@gmail.com>
2021-07-08 10:50:15 -0700

    add article

--- data/press.js
+++ data/press.js
@@ -28,6 +28,15 @@ const highlights = [
 
 const press = [
   {
+    source: 'MIT Tech Review',
+    href: 'https://www.technologyreview.com/2021/07/08/1027908/carbon-removal-hype-is-a-dangerous-distraction-climate-change/',
+    title:
+      'Carbon removal hype is becoming a dangerous distraction',
+    authors: ['James Temple'],
+    date: '07-08-2021',
+    format: 'print',
+  },
+  {
     source: 'Grist',
     href: 'https://grist.org/agriculture/us-carbon-removal-capture-offset-forests-farms-trees-soil/',
     title:
