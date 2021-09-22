943fcd101046fcc5e1470ba198ef9cb8dcec01d5
Kata Martin <katasm@gmail.com>
2021-09-22 10:07:27 -0700

    Remove extra babel config

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,11 @@ loadEnvConfig(process.env.PWD)
 const config = {
   verbose: true,
   setupFiles: ['./jest.setup.js'],
+  transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 }
 
 module.exports = config
