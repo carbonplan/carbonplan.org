6ef7282c63974b8a7507f9935fbe63e3ec9f3097
Kata Martin <katasm@gmail.com>
2021-11-08 16:07:43 -0800

    Combine recaptcha and checkout_sessions into single endpoint

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -10,48 +10,92 @@ const limiter = rateLimit({
   uniqueTokenPerInterval: 100, // Max 100 users per hour
 })
 
-export default async function handler(req, res) {
+async function recaptchaHandler(response) {
+  if (!response) {
+    return {
+      success: false,
+      statusCode: 400,
+      message: 'Recaptcha response missing',
+    }
+  }
+
+  const result = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${response}`,
+    {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      referrerPolicy: 'no-referrer',
+    }
+  )
+  const { success, 'error-codes': errorCodes } = await result.json()
+
+  if (!success) {
+    console.warn('recaptcha failed', errorCodes)
+  }
+
+  return { success }
+}
+
+async function checkoutHandler(amount) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
+  // Validate the amount that was passed from the client.
+  if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
+    return { success: false, statusCode: 400, message: 'Invalid amount' }
+  }
+
+  // Create Checkout Sessions from body params.
+  const params = {
+    submit_type: 'donate',
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        name: 'Donation',
+        description: 'Donation to CarbonPlan',
+        amount: amount * 100, // Stripe accepts amount in cents
+        currency: CURRENCY,
+        quantity: 1,
+      },
+    ],
+    success_url: 'https://carbonplan.org/thanks',
+    cancel_url: 'https://carbonplan.org/donate',
+  }
+  const checkoutSession = await stripe.checkout.sessions.create(params)
+  return { success: true, checkoutSession }
+}
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
+    const { recaptcha, amount } = req.body
     try {
-      await limiter.check(res, 10, req.headers['x-forwarded-for']) // 10 requests per hour per IP
-    } catch {
-      res.status(429).json({ statusCode: 429, error: 'Rate limit exceeded' })
-      return
-    }
-
-    const { amount, vercel_url } = req.body
-    try {
-      // Validate the amount that was passed from the client.
-      if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
-        throw new Error('Invalid amount.')
-      }
-
-      if (vercel_url !== process.env.VERCEL_URL) {
-        res.status(400).json({ statusCode: 400, error: 'Missing Vercel URL' })
+      const recaptchaResult = await recaptchaHandler(recaptcha)
+      if (!recaptchaResult.success) {
+        res.status(400).json({ statusCode: 400, message: 'Recaptcha failed' })
         return
       }
 
-      // Create Checkout Sessions from body params.
-      const params = {
-        submit_type: 'donate',
-        payment_method_types: ['card'],
-        line_items: [
-          {
-            name: 'Donation',
-            description: 'Donation to CarbonPlan',
-            amount: amount * 100, // Stripe accepts amount in cents
-            currency: CURRENCY,
-            quantity: 1,
-          },
-        ],
-        success_url: 'https://carbonplan.org/thanks',
-        cancel_url: 'https://carbonplan.org/donate',
+      try {
+        await limiter.check(res, 10, req.headers['x-forwarded-for']) // 10 requests per hour per IP
+      } catch {
+        res
+          .status(429)
+          .json({ statusCode: 429, message: 'Rate limit exceeded' })
+        return
       }
-      const checkoutSession = await stripe.checkout.sessions.create(params)
+
+      const { success, statusCode, message, checkoutSession } =
+        await checkoutHandler(amount, req, res)
+      if (!success) {
+        res.status(statusCode).json({ statusCode, message })
+        return
+      }
+
       res.status(200).json(checkoutSession)
     } catch (err) {
+      console.log(err.message)
       res.status(500).json({ statusCode: 500, message: err.message })
     }
   } else {
