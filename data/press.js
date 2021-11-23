f0c2fb2c07bf90d8a1c3774aefc0952971f53531
Becky Hurst <82540542+beckyhurst@users.noreply.github.com>
2021-11-23 10:06:23 -0800

    Update press.js

--- data/press.js
+++ data/press.js
@@ -28,7 +28,7 @@ const highlights = [
 
 const press = [
   {
-    source: ['The New York Times'],
+    source: ['New York Times'],
     href: 'https://www.nytimes.com/2021/11/23/business/dealbook/farm-carbon-credits.html',
     title: 'The latest farm product: Carbon credits',
     authors: ['Elizabeth G. Dunn'],
