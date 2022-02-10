f90f42c05630638a3c0bf97403e8d72b65aabe90
Becky Hurst <82540542+beckyhurst@users.noreply.github.com>
2022-02-10 11:51:20 -0800

    Update funding.js

--- data/funding.js
+++ data/funding.js
@@ -69,6 +69,7 @@ const unrestricted = [
   ],
   ['Calvin French-Owen', 'Individual'],
   ['Additional Ventures', 'Foundation'],
+  ['Rovere O\'Kelley Family Fund', 'Individual'],
 ]
 
 const projectSpecific = [
