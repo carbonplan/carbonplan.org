b513ff7b6143d048b090dc4be8686f3bfe9e2153
Kata Martin <katasm@gmail.com>
2022-05-04 16:05:51 -0700

    Update recent press list on landing page

--- data/recent.js
+++ data/recent.js
@@ -39,20 +39,20 @@ const highlights = [
 
 const press = [
   {
-    label: 'Grist',
-    href: 'https://grist.org/wildfires/california-forests-carbon-offsets-reduce-emissions/',
+    label: 'National Geographic',
+    href: 'https://www.nationalgeographic.com/environment/article/forests-as-carbon-offsets-climate-change-has-other-plans',
   },
   {
     label: 'Los Angeles Times',
     href: 'https://www.latimes.com/politics/story/2021-09-08/what-is-the-california-climate-credit-does-it-cut-pollution',
   },
   {
-    label: 'Bloomberg',
-    href: 'https://www.bloomberg.com/news/features/2021-08-11/the-fictitious-world-of-carbon-neutral-fossil-fuel?srnd=green',
-  },
-  {
     label: 'Planet Money',
     href: 'https://www.npr.org/2021/05/07/994774148/emission-impossible',
+  },
+  {
+    label: 'Grist',
+    href: 'https://grist.org/wildfires/california-forests-carbon-offsets-reduce-emissions/',
   },
 ]
 
