94ccb03ae53c276e47f610726afd7b5de08fe250
sage-ortiz <109317953+sage-ortiz@users.noreply.github.com>
2022-09-07 09:52:18 -0700

    Update press.js

--- data/press.js
+++ data/press.js
@@ -36,6 +36,15 @@ const press = [
     date: '09-08-2022',
     format: 'audio',
   },
+    {
+    source: ['Los Amgeles Times'],
+    href: 'https://www.latimes.com/environment/story/2022-09-06/can-california-deliver-on-its-zero-emission-car-goal',
+    title:
+      'California vows to ban gas-fueled cars. But its record on big climate promises is mixed',
+    authors: ['Hayley Smith', 'Tony Briscoe'],
+    date: '09-06-2022',
+    format: 'print',
+  },
   {
     source: ['New York Times'],
     href: 'https://www.nytimes.com/2022/09/01/climate/california-lawmakers-climate-legislation.html?partner=slack&smid=sl-share',
