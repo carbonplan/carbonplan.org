5a48d3bfb4518c72d86899f9c8ef803b014b2dc0
freeman-lab <the.freeman.lab@gmail.com>
2022-12-04 17:58:49 -0800

    team description update

--- data/team.js
+++ data/team.js
@@ -65,7 +65,7 @@ const team = [
   {
     name: 'Max Jones',
     role: 'Machine Learning Engineer',
-    bio: 'Max is a data scientist focused on machine learning, cloud computing, and open source software development. They are excited to build tools that enable climate action. Max holds a PhD from the Massachusetts Institute of Technology — Woods Hole Oceanographic Institution Joint Program in Oceanography.',
+    bio: 'Max is a data scientist focused on machine learning, cloud computing, and open source software development. He is excited to build tools that enable climate action. Max holds a PhD from the Massachusetts Institute of Technology — Woods Hole Oceanographic Institution Joint Program in Oceanography.',
   },
   {
     name: 'Sage Ortiz',
