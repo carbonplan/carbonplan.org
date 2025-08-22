98e6e7aa295db3425c7d0f4a64368655d9864492
Kata Martin <katasm@gmail.com>
2025-08-22 11:47:34 -0400

    Update team page

--- data/team.js
+++ data/team.js
@@ -45,11 +45,6 @@ const team = [
     bio: 'Tyler is a geochemist and climate scientist passionate about climate action that is rooted in a deep understanding of Earth system dynamics. He holds a PhD in Geological Sciences from Stanford University and a BA in Earth and Planetary Sciences from Northwestern University.',
   },
   {
-    name: 'Maggie Koerth',
-    role: 'Editorial Lead',
-    bio: 'Maggie is an award-winning journalist with 20 years’ experience covering the intersection of science and society for such publications as FiveThirtyEight.com and The New York Times Magazine. She is a Harvard Nieman Fellow (’15) and Berkman Klein Fellow and a member of the board at the Council for the Advancement of Science Writing.',
-  },
-  {
     name: 'Shane Loeffler',
     role: 'Software Engineer',
     bio: 'Shane is a software engineer with a background in the Earth Sciences. He’s excited to make maps, visualizations, and tools that aid in fights for environmental justice and climate action. He holds an MS in Earth Sciences from the University of Minnesota.',
