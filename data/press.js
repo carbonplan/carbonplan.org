436b18276d6663d875f10324371e0b92286516e8
Becky Hurst <82540542+beckyhurst@users.noreply.github.com>
2025-12-18 12:08:17 -0800

    Update press.js

--- data/press.js
+++ data/press.js
@@ -33,7 +33,7 @@ const press = [
     source: ['Climate Home News'],
     href: 'https://www.climatechangenews.com/2025/12/18/house-of-cards-verra-used-junk-carbon-credits-to-fix-shells-offsetting-scandal/',
     title:
-      '“House of cards”: Verra used junk carbon credits to fix Shell’s offsetting scandal',
+      'Verra used junk carbon credits to fix Shell’s offsetting scandal',
     authors: ['Matteo Civillini'],
     date: '12-18-25',
     format: 'print',
